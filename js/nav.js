5ae1539d79fdfa3f9edb2aad8b9f975d9251bbe9
markstrathie <strathiemark@gmail.com>
2024-04-01 21:03:43 +0100

    Added js for nav bar scrolling and changed homepage image

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,5 @@
+// Enable curtain nav bar for mobile
+
 const primaryNav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 
@@ -12,3 +14,24 @@ navToggle.addEventListener('click', () => {
         navToggle.setAttribute('aria-expanded', false);
     }
 })
+
+// Enable hidden nav bar
+
+const nav = document.querySelector(".nav");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+    if (lastScrollY < window.scrollY) {
+        nav.classList.add("nav--hidden");
+    } else {
+        nav.classList.remove("nav--hidden");
+    }
+
+    lastScrollY = window.scrollY;
+    if (window.scrollY <= 0) {
+
+        nav.classList.remove("nav--hidden")
+
+    }
+
+});
